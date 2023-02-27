8f3378742efa7df06402fdc42e0d3b9ae7af7bf1
yxj <564197835@qq.com>
2023-02-27 12:47:35 +0800

    better structure

--- my_mosse.py
+++ my_mosse.py
@@ -10,6 +10,7 @@ Python re-implementation of "Visual Object Tracking using Adaptive Correlation F
 Dongmen practices at 2020/3/26.
 """
 
+from typing import Tuple
 import numpy as np
 import cv2
 
@@ -61,79 +62,93 @@ class MOSSE(BaseCF):
 
     # first_frame: np.array
     # bbox: x y w h
-    def init(self, first_frame, bbox):
+    def init(self, first_frame: np.ndarray, bbox: Tuple[int]):
         if len(first_frame.shape) != 2:
             assert first_frame.shape[2] == 3
             first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)  # RGB图片转换成灰度图片
         first_frame = first_frame.astype(np.float32) / 255  # 归一化
         x, y, w, h = bbox  # 取出第一帧中ground truth的坐标值 x,y为框的左上角坐标，w, h为框的大小
-        self._center = (x + w / 2, y + h / 2)  # 计算ground truth的中心
+        self.center = (x + w / 2, y + h / 2)  # 计算ground truth的中心
         self.w, self.h = w, h  # 获取框的大小
         w, h = int(round(w)), int(round(h))  # round()四舍五入
         self.cos_window = cos_window((w, h))  # 定义汉宁窗
         # 从第一帧中截取出检测的目标部分， _fi.shape = (w,h)
         self._fi = cv2.getRectSubPix(
-            image=first_frame, patchSize=(w, h), center=self._center
+            image=first_frame, patchSize=(w, h), center=self.center
         )
         # 首先生成一个w * h(检测框大小)的高斯核，然后对该高斯核进行傅里叶变换，初始化G
         self._G = np.fft.fft2(gaussian2d_labels((w, h), self.sigma))
         self.crop_size = (w, h)  # 定义裁剪框的大小
-        self._Ai = np.zeros_like(self._G)  # 初始化Ai
-        self._Bi = np.zeros_like(self._G)  # 初始化Bi
+        self.Ai = np.zeros_like(self._G)  # 初始化Ai
+        self.Bi = np.zeros_like(self._G)  # 初始化Bi
         # 对Fi进行多次刚性形变，增强检测的鲁棒性
         # 计算出Ai和Bi的初始值
         for _ in range(8):
-            fi = self._rand_warp(self._fi)  # YXJ: 如果测试场景不发生大小变化、旋转等，不需要做
+            fi = self.randomWarp(self._fi)  # YXJ: 如果测试场景不发生大小变化、旋转等，不需要做
             # fi = self._fi
-            Fi = np.fft.fft2(self._preprocessing(fi, self.cos_window))
-            self._Ai += self._G * np.conj(Fi)
-            self._Bi += Fi * np.conj(Fi)
+            Fi = np.fft.fft2(self.preprocessing(fi, self.cos_window))
+            self.Ai += self._G * np.conj(Fi)
+            self.Bi += Fi * np.conj(Fi)
+        self.Hi = self.Ai / self.Bi
 
-    def update(self, current_frame, vis=False) -> list[int]:
+    def update(self, current_frame, vis=False) -> tuple[int]:
+        # [获取新的一帧]
+
         if len(current_frame.shape) != 2:
             assert current_frame.shape[2] == 3
             current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         current_frame = current_frame.astype(np.float32) / 255
-        Hi = self._Ai / self._Bi
+
+        # [寻找新一帧中的框]
+
+        # 针对当前帧，用前一个目标框的中心截取一个框
         fi = cv2.getRectSubPix(
-            current_frame, (int(round(self.w)), int(round(self.h))), self._center
-        )  # 针对当前帧，用前一个目标框的中心截取一个框
-        fi = self._preprocessing(fi, self.cos_window)
-        Gi = Hi * np.fft.fft2(fi)
-        gi = np.real(np.fft.ifft2(Gi))  # 对频域下的Gi进行逆傅里叶变换得到实际的gi
+            current_frame, (int(round(self.w)), int(round(self.h))), self.center
+        )
+        fi = self.preprocessing(fi, self.cos_window)
+        # 卷积得到Gi
+        Gi = self.Hi * np.fft.fft2(fi)
+        # 对频域下的Gi进行逆傅里叶变换得到实际的gi
+        gi = np.real(np.fft.ifft2(Gi))  
         if vis is True:
             self.score = gi
-        curr = np.unravel_index(
-            np.argmax(gi, axis=None), gi.shape
-        )  # 获取gi中最大值的index，这个位置就是第二帧图像中目标所在
-        dy, dx = curr[0] - (self.h / 2), curr[1] - (self.w / 2)
-        x_c, y_c = self._center
-        x_c += dx
-        y_c += dy
-        self._center = (x_c, y_c)  # 此处得到新框的中心的坐标
-        # 针对当前帧，用新的中心截取一个框
+        # 获取gi中最大值的index，这个位置就是第二帧图像中目标所在（算法假设物体不会运动超过上个框）
+        position = np.unravel_index(np.argmax(gi, axis=None), gi.shape)
+        dy, dx = position[0] - (self.h / 2), position[1] - (self.w / 2)
+        x_c, y_c = self.center
+        self.center = (x_c + dx, y_c + dy)  
+
+        # [更新核]
+
+        # 用新的框截出物体
         fi = cv2.getRectSubPix(
-            current_frame, (int(round(self.w)), int(round(self.h))), self._center
+            current_frame, (int(round(self.w)), int(round(self.h))), self.center
         )
-        fi = self._preprocessing(fi, self.cos_window)
+        fi = self.preprocessing(fi, self.cos_window)
         Fi = np.fft.fft2(fi)
-        self._Ai = (
+        # 使用学习学习率更新 Hi
+        self.Ai = (
             self.interp_factor * (self._G * np.conj(Fi))
-            + (1 - self.interp_factor) * self._Ai
+            + (1 - self.interp_factor) * self.Ai
         )
-        self._Bi = (
-            self.interp_factor * (Fi * np.conj(Fi))
-            + (1 - self.interp_factor) * self._Bi
+        self.Bi = (
+            self.interp_factor * (Fi * np.conj(Fi)) + (1 - self.interp_factor) * self.Bi
         )
-        # 返回值为当前框的坐标(x, y, w, h)
-        return [
-            self._center[0] - self.w / 2,
-            self._center[1] - self.h / 2,
+        self.Hi = self.Ai / self.Bi
+
+        # [返回]
+
+        # 返回新一帧的框 (x, y, w, h)
+        return (
+            self.center[0] - self.w / 2,
+            self.center[1] - self.h / 2,
             self.w,
             self.h,
-        ]
+        )
 
-    def _preprocessing(self, img, cos_window, eps=1e-5):
+    def preprocessing(
+        self, img: np.ndarray, cos_window: np.ndarray, eps=1e-5
+    ) -> np.ndarray:
         """
         该函数对数据进行预处理：
         1. 对数据矩阵取对数
@@ -145,7 +160,7 @@ class MOSSE(BaseCF):
         img = (img - np.mean(img)) / (np.std(img) + eps)
         return cos_window * img
 
-    def _rand_warp(self, img):
+    def randomWarp(self, img: np.ndarray) -> np.ndarray:
         """
         该函数对第一帧的目标框进行随机重定位，刚性形变，减轻漂移。
         """
