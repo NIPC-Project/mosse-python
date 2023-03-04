d433b9a9a463e9395b3fc2d87a586a3cd008163f
yxj <564197835@qq.com>
2023-03-04 20:43:07 +0800

    use boxed functions

--- mosse_yxj.py
+++ mosse_yxj.py
@@ -1,27 +1,10 @@
-"""
-Python re-implementation of "Visual Object Tracking using Adaptive Correlation Filters"
-@inproceedings{Bolme2010Visual,
-  title={Visual object tracking using adaptive correlation filters},
-  author={Bolme, David S. and Beveridge, J. Ross and Draper, Bruce A. and Lui, Yui Man},
-  booktitle={Computer Vision & Pattern Recognition},
-  year={2010},
-}
-
-Dongmen practices at 2020/3/26.
-"""
-
 import numpy as np
 import cv2
 
 
-# 汉宁窗，防止频谱泄露
 def cosWindow(size: tuple[int, int]) -> np.ndarray:
     """
-    width, height = sz
-    j = np.arange(0, width)
-    i = np.arange(0, height)
-    J, I = np.meshgrid(j, i)
-    cos_window = np.sin(np.pi * J / width) * np.sin(np.pi * I / height)
+    汉宁窗，防止频谱泄露
     """
     cos_window = np.hanning(int(size[1]))[:, np.newaxis].dot(
         np.hanning(int(size[0]))[np.newaxis, :]
@@ -31,7 +14,7 @@ def cosWindow(size: tuple[int, int]) -> np.ndarray:
 
 def gaussianKernel(size: tuple[int, int], sigma: float) -> np.ndarray:
     """
-    该函数的作用是生成一个sz大小的的高斯核
+    高斯核
     """
     w, h = size
     xs, ys = np.meshgrid(np.arange(w), np.arange(h))  # 根据w, h的值生成一个网格的x，y坐标
@@ -39,6 +22,28 @@ def gaussianKernel(size: tuple[int, int], sigma: float) -> np.ndarray:
     dist = ((xs - center_x) ** 2 + (ys - center_y) ** 2) / (sigma**2)
     labels = np.exp(-0.5 * dist)
     return labels
+
+
+def convertImageToFloat(image: np.ndarray) -> np.ndarray:
+    """
+    将彩色 8 bit 图像转为灰度 0.0-1.0
+    """
+    if len(image.shape) != 2:
+        assert image.shape[2] == 3
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 归一化
+    image = image.astype(np.float32) / 255
+    return image
+
+
+def preprocessing(img: np.ndarray, cos_window: np.ndarray, eps=1e-5) -> np.ndarray:
+    img = (img - np.mean(img)) / (np.std(img) + eps)
+    return cos_window * img
+
+
+def getSubImage(image: np.ndarray, bbox: tuple[int, int, int, int]) -> np.ndarray:
+    x, y, w, h = bbox
+    return cv2.getRectSubPix(image, (w, h), (x + w / 2, y + h / 2))
 
 
 class BaseCF:
@@ -59,59 +64,40 @@ class MOSSE(BaseCF):
         self.sigma: sigma = sigma  # 高斯变换中的方差
 
     def init(self, first_frame: np.ndarray, bbox: tuple[int, int, int, int]):
-        # [取出第一帧]
-
-        # RGB图片转换成灰度图片
-        if len(first_frame.shape) != 2:
-            assert first_frame.shape[2] == 3
-            first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
-        # 归一化
-        first_frame = first_frame.astype(np.float32) / 255
+        first_frame = convertImageToFloat(image=first_frame)
 
         # [保存一些之后用到的变量]
 
         # 取出第一帧中ground truth的坐标值 x,y为框的左上角坐标，w, h为框的大小
-        x, y, w, h = bbox
-        # 计算ground truth的中心
-        self.center: tuple[float, float] = (x + w / 2, y + h / 2)
+        self.x, self.y, self.w, self.h = bbox
         # 获取框的大小
-        self.w, self.h = w, h
-        self.crop_size = (w, h)  # 定义裁剪框的大小
+        self.crop_size = (self.w, self.h)  # 定义裁剪框的大小
         # 定义汉宁窗
-        self.cos_window = cosWindow(size=(w, h))
+        self.cos_window = cosWindow(size=(self.w, self.h))
 
         # [使用随机变换的groundtruth进行训练得到初始的核]
 
         # 从第一帧中截取出检测的目标部分
-        fi_groundtruth = cv2.getRectSubPix(
-            image=first_frame, patchSize=(w, h), center=self.center
-        )
+        fi_groundtruth = getSubImage(first_frame, (self.x, self.y, self.w, self.h))
         # 首先生成一个w * h(检测框大小)的高斯核，然后对该高斯核进行傅里叶变换，初始化G
-        self._G = np.fft.fft2(gaussianKernel(size=(w, h), sigma=self.sigma))
+        self._G = np.fft.fft2(gaussianKernel(size=(self.w, self.h), sigma=self.sigma))
         self.Ai = np.zeros_like(self._G)
         self.Bi = np.zeros_like(self._G)
         # 训练初始的核
         fi = fi_groundtruth
-        Fi = np.fft.fft2(self.preprocessing(fi, self.cos_window))
+        Fi = np.fft.fft2(preprocessing(fi, self.cos_window))
         self.Ai += self._G * np.conj(Fi)
         self.Bi += Fi * np.conj(Fi)
         self.Hi = self.Ai / self.Bi
 
     def update(self, current_frame, vis=False) -> tuple[int]:
-        # [获取新的一帧]
-
-        if len(current_frame.shape) != 2:
-            assert current_frame.shape[2] == 3
-            current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-        current_frame = current_frame.astype(np.float32) / 255
+        current_frame = convertImageToFloat(current_frame)
 
         # [寻找新一帧中的框]
 
         # 针对当前帧，用前一个目标框的中心截取一个框
-        fi = cv2.getRectSubPix(
-            current_frame, (int(round(self.w)), int(round(self.h))), self.center
-        )
-        fi = self.preprocessing(fi, self.cos_window)
+        fi = getSubImage(image=current_frame, bbox=(self.x, self.y, self.w, self.h))
+        fi = preprocessing(fi, self.cos_window)
         # 卷积得到Gi
         Gi = self.Hi * np.fft.fft2(fi)
         # 对频域下的Gi进行逆傅里叶变换得到实际的gi
@@ -121,16 +107,13 @@ class MOSSE(BaseCF):
         # 获取gi中最大值的index，这个位置就是第二帧图像中目标所在（算法假设物体不会运动超过上个框）
         position = np.unravel_index(np.argmax(gi, axis=None), gi.shape)
         dy, dx = position[0] - (self.h / 2), position[1] - (self.w / 2)
-        x_c, y_c = self.center
-        self.center = (x_c + dx, y_c + dy)
+        self.x, self.y = (self.x + dx, self.y + dy)
 
         # [更新核]
 
         # 用新的框截出物体
-        fi = cv2.getRectSubPix(
-            current_frame, (int(round(self.w)), int(round(self.h))), self.center
-        )
-        fi = self.preprocessing(fi, self.cos_window)
+        fi = getSubImage(image=current_frame, bbox=(self.x, self.y, self.w, self.h))
+        fi = preprocessing(fi, self.cos_window)
         Fi = np.fft.fft2(fi)
         # 使用学习学习率更新 Hi
         self.Ai = (
@@ -146,22 +129,8 @@ class MOSSE(BaseCF):
 
         # 返回新一帧的框 (x, y, w, h)
         return (
-            self.center[0] - self.w / 2,
-            self.center[1] - self.h / 2,
+            self.x,
+            self.y,
             self.w,
             self.h,
         )
-
-    def preprocessing(
-        self, img: np.ndarray, cos_window: np.ndarray, eps=1e-5
-    ) -> np.ndarray:
-        """
-        该函数对数据进行预处理：
-        1. 对数据矩阵取对数
-        2. 接着标准化数据，使其更加符合标准正态分布
-        (经过以上两步处理，直观上来说数据变得中心化了，弱化了其背景的影响)
-        3. 使用窗函数处理数据，减弱其频谱泄露现象
-        """
-        img = np.log(img + 1)
-        img = (img - np.mean(img)) / (np.std(img) + eps)
-        return cos_window * img
