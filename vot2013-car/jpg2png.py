6984fc89b71040afc0cfb589e9ee2576bb144203
yxj <564197835@qq.com>
2023-04-19 15:36:08 +0800

    rename

--- vot2013-car/jpg2png.py
+++ vot2013-car/jpg2png.py
@@ -1,6 +1,6 @@
 import cv2
 
 for i in range(1, 375):
-    frame = cv2.imread(f"frames/{i:08}.jpg")
+    frame = cv2.imread(f"frames-rgb/{i:08}.jpg")
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"frames-png-gray/{i}.png", frame_gray)
+    cv2.imwrite(f"frames-gray-png/{i}.png", frame_gray)
