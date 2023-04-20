fcb7c116a4d34b05ed4ff2a07300fb759dd63013
yxj <564197835@qq.com>
2023-04-21 07:25:13 +0800

    update hanning_window

--- unit/hanning_window.py
+++ unit/hanning_window.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-w = 64
+w = 32
 h = 32
 
-result_np = np.hanning(h)[:, np.newaxis].dot(np.hanning(w)[np.newaxis, :])
+result_np = np.array(np.hanning(h)[:, np.newaxis].dot(np.hanning(w)[np.newaxis, :]))
 result_yxj = np.zeros((h, w))
 
 hanning_w = np.zeros(w)
@@ -17,7 +17,7 @@ for r in range(h):
     for c in range(w):
         result_yxj[r][c] = hanning_h[r] * hanning_w[c]
 
-# print(result_np)
+print(result_np.reshape(1, 1024).tolist())
 # print(result_yxj)
-print(result_np - result_yxj)
+# print(result_np - result_yxj)
 print(hanning_w)
