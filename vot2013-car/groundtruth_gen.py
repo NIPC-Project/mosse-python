6501cfdf986cd6176f3a16c91e1a29d6aa72fa16
yxj <564197835@qq.com>
2023-04-27 07:59:22 +0800

    add groundtruth center for car series

--- vot2013-car/groundtruth_gen.py
+++ vot2013-car/groundtruth_gen.py
@@ -1,7 +1,11 @@
 import csv
 
 groundtruth = []
-with open("info/groundtruth.txt") as f:
+with open("info/groundtruth.txt", "r") as f:
     for row in csv.reader(f):
         groundtruth.append([(int(float(i))) for i in row])
-print("# [x, y, w, h]\nvot2013_car_groundtruth = ", groundtruth)
+groundtruth_center = [[int(x + w / 2), int(y + h / 2)] for [x, y, w, h] in groundtruth]
+with open("groundtruth.py", "w") as f:
+    f.write(f"# [x, y, w, h]\nvot2013_car_groundtruth = {groundtruth}\n")
+    f.write("\n")
+    f.write(f"# [xc, yc]\nvot2013_car_groundtruth_center = {groundtruth_center}\n")
